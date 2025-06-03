ac302063b473ba5ef95f46719982d9b99e33c571
Guille <lunaalvarezguillermo96@gmail.com>
2025-06-04 01:49:09 +0200

    Agregado endpoint saludo con nombre como parametro

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+
+app.get('/api/v1/saludo', (req, res) => {
+  const nombre = req.query.nombre;
+
+  if (!nombre) {
+    return res.status(400).json({ mensaje: "Falta el parámetro 'nombre' en la consulta" });
+  }
+
+  res.status(200).json({ mensaje: `Hola, ${nombre}!` });
+});
